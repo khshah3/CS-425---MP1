bc808b87f00495f299b38f98b4cd8e5584f5d9fc
Karan Shah <admin@Admins-MacBook-Air.local>
2013-09-13 22:09:31 -0500

    Initial Commi

---
+++ TCPClient.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net"
+    "encoding/gob"
+)
+
+type P struct {
+    M, N int64
+}
+
+func main() {
+    fmt.Println("start client");
+    conn, err := net.Dial("tcp", "localhost:8080")
+    if err != nil {
+        log.Fatal("Connection error", err)
+    }
+    encoder := gob.NewEncoder(conn)
+    p := &P{1, 2}
+    fmt.Println(p);
+    encoder.Encode(p)
+    conn.Close()
+    fmt.Println("done");
+}
+
